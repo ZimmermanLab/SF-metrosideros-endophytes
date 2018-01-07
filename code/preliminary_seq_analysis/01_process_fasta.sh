0e736d1f9ca9e7e788254cd325a5aba0f2ad6df9
Naupaka Zimmerman <naupaka@gmail.com>
2018-01-06 23:47:38 -0800

    Add comments and TODOs to seq_analysis scripts

--- code/preliminary_seq_analysis/01_process_fasta.sh
+++ code/preliminary_seq_analysis/01_process_fasta.sh
@@ -1,6 +1,20 @@
+#!/bin/bash
+
+# Draft of bash script to clean ITS fasta sequences
+# and cluster them using mothur
+
+# Originally written by Naupaka Zimmerman
+# nzimmerman@usfca.edu
+# August 24, 2017
+
+## TODO: Need to add echo statements to let use know what is happening
+## TODO: Need to adjust paths to new directory structure, or use ARGV
+
+# combine all individual fasta files into single combined fasta
 cat *.fasta > all_seqs.fasta
 
 # count number of non-failed sequences
+# need to install bioawk if it is not already installed
 bioawk -c fastx '!/FAILED/ && !/FALIED/ {print ">"$name"\n"$seq}' all_seqs.fasta | grep -c ">"
 
 # get rid of failed sequences and all but first EUSF00001 sequence
@@ -23,9 +37,9 @@ grep "EUSF" good_seqs_short_names.fasta | sort | uniq -c | sort | tail
 bioawk -c fastx '!/EUSF00462/ {print ">"$name"\n"$seq}' good_seqs_short_names.fasta > good_seqs_short_names_checked.fasta
 
 # cluster using vsearch
+# need to install mothur if it is not already installed
 mothur "#cluster(fasta=good_seqs_short_names_checked.fasta, method=agc);"
 
 mothur "#bin.seqs(list=good_seqs_short_names_checked.agc.list, fasta=good_seqs_short_names_checked.fasta);"
 
 grep ">" good_seqs_short_names_checked.agc.0.03.fasta | sed 's/>//' > seq_with_OTU_ID.txt
-
