fb22698221549b0875b154233e48811a082394b8
emmagibson <egibson@dons.usfca.edu>
2018-04-10 11:28:58 -0700

    Isolate extraction numbers for failed sequences

--- find_failed_seqs.R
+++ find_failed_seqs.R
@@ -1,7 +1,13 @@
+# Draft of R script to perform some ecological analyses on clustered data
+# originating from single Sanger ITS reads
+
+# Originally written by Emma Gibson
+# egibson@dons.usfca.edu
+# April 5, 2018
 
 library(seqinr)
-fasta_in <- read.fasta("data/preliminary_trimmed_seqs.fasta")
-seq_names <- names(fasta_in)
-failed_seqs <- seq_names[grepl(pattern = "_A10", seq_names)]
-
-#gsub - cleans out info you don't need
+hand_fasta_in <- read.fasta("hand-cleaned_seqs/hand-cleaned_seqs.fasta")
+seq_names <- names(hand_fasta_in)
+failed_seqs <- seq_names[grepl(pattern = "_failed", seq_names)]
+failed_seqs <- failed_seqs[grepl(pattern = "EUSF", failed_seqs)]
+failed_seqs_names_only <- gsub("_ITS1F_[A-H][0-9]+_failed.ab1","",failed_seqs)
