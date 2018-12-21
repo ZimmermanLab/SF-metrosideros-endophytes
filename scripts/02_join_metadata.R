f3dba93a394ac07fef8bd6fb6734c1d323ad2f52
Naupaka Zimmerman <naupaka@gmail.com>
2018-12-20 18:57:07 -0800

    Add first draft of recursive function to find tree

--- scripts/02_join_metadata.R
+++ scripts/02_join_metadata.R
@@ -11,9 +11,28 @@
 library("dplyr")
 
 # load data
-culturing <- read.csv("data/metadata/culturing_worksheet.csv")
-extractions <- read.csv("data/metadata/Extraction_worksheet.csv")
-trees <- read.csv("data/metadata/M_excel_tree_metadata_with_isolationfreq.csv")
+culturing <- read.csv("data/metadata/culturing_worksheet.csv",
+                      stringsAsFactors = FALSE)
+extractions <- read.csv("data/metadata/Extraction_worksheet.csv",
+                        stringsAsFactors = FALSE)
+trees <- read.csv("data/metadata/M_excel_tree_metadata_with_isolationfreq.csv",
+                  stringsAsFactors = FALSE)
+
+
+# goal is to find the highest level parent of a given culture
+# we accomplish this for an arbitraty number of parents using a
+# recursive function
+find_top_level_parent_of_culture <- function(culture_id) {
+  if (culture_id %in% culturing$Label_ID) {
+    find_top_level_parent_of_culture(
+      culturing[culturing$Label_ID == culture_id, "From_Label_ID"])
+  }
+  else {
+    return(culture_id)
+  }
+}
+
+stopifnot(find_top_level_parent_of_culture("ZUSF02440") == "S5T3")
 
 # 4 levels of joining since we can't recursively join
 joined <- extractions %>%
