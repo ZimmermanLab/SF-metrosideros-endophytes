bb9de33758d796fe9d264c338c519e824b774e63
Naupaka Zimmerman <naupaka@gmail.com>
2021-10-29 02:35:51 +0000

    Remove remaining preliminary files

--- analyses/blast_analyses/preliminary_seq_BLAST-sum.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-#Emma Gibson, February 22, 2018
-#a quick, temporary script to count the species abundances in preliminary sequences
-
-#make a file that summarizes the biggest groups in the preliminary dataset
-echo "counting most abundant species..."
-cut -d, -f1 BLAST_results/prelim_BLAST.csv | sort | uniq -c | sort -n > BLAST_results/2018-02-20_prelim_BLAST_summary.txt
-echo "finished counting most abundant species"
