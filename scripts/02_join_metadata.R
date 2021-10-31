4af78b92b15893651902906c570c191d5472c133
Naupaka Zimmerman <naupaka@gmail.com>
2021-10-31 08:18:57 +0000

    Major cleanup of lookup functions

--- scripts/02_join_metadata.R
+++ scripts/02_join_metadata.R
@@ -9,41 +9,78 @@
 # load packages
 library("dplyr")
 
-# set debug -- if TRUE, will output more information for checking assumptions
-debug <- FALSE
 
 # load data
-culturing <- read.csv("data/metadata/culturing_worksheet.csv",
+cultures <- read.csv("data/metadata/culturing_worksheet_emma.csv",
                       stringsAsFactors = FALSE)
-extractions <- read.csv("data/metadata/extraction_worksheet.csv",
+
+extractions <- read.csv("data/metadata/extraction_worksheet_emma.csv",
                         stringsAsFactors = FALSE)
+
 trees <- read.csv("data/metadata/m_excel_tree_metadata_with_isolationfreq.csv",
                   stringsAsFactors = FALSE)
 
+vouchers <- read.csv("data/metadata/water_voucher_worksheet_emma.csv",
+                     stringsAsFactors = FALSE)
 
-# goal is to find the highest level parent of a given culture
-# we accomplish this for an arbitraty number of parents using a
-# recursive function
-find_top_level_parent <- function(culture_id) {
-  if (culture_id %in% culturing$Label_ID) {
-    find_top_level_parent(
-      culturing[culturing$Label_ID == culture_id, "From_Label_ID"])
+seq_ids_passing_checks <- read.table("output/mothur_pipeline/05_seq_names.txt",
+                                     col.names = "seq_id")
+
+# get rid of data entry typo row
+extractions <- extractions[extractions$Culture_Label_ID != "ZUSF002481", ]
+
+# function to check if a parent exists
+# 'exists' in this case means was part of this project and so is
+# included in the metadata files
+check_if_parent_exists <- function(parent_culture) {
+  
+  # if the parent is missing from the matching below, then it will
+  # return a character vector of length 0, which we check for here
+  if (length(parent_culture) > 0) {
+    return(parent_culture)
   }
   else {
-    return(culture_id)
+    return("Not found")
+  }
+}
+
+# look up the culture a voucher came from in the voucher table
+find_culture_for_voucher <- function(voucher_id) {
+  check_if_parent_exists(vouchers$Label_ID_culture[
+    vouchers$Label_ID_Voucher == voucher_id])
+
+}
+
+# goal is to find the highest level parent of a given culture
+# we accomplish this for an arbitrary number of parents using a
+# recursive function
+find_top_level_parent <- function(culture_id) {
+  
+  # if it comes from a voucher, need to use the function for that
+  if (grepl(pattern = "VUSF", x = culture_id)) {
+    find_top_level_parent(find_culture_for_voucher(culture_id))
+  }
+  
+  # if the culture exists, find its parent
+  else if (culture_id %in% cultures$Label_ID) {
+    find_top_level_parent(
+      check_if_parent_exists(cultures[
+        cultures$Label_ID == culture_id, "From_Label_ID"]))
+  }
+  else {
+      return(culture_id)     
   }
 }
 
 # test to make sure the function works
 stopifnot(find_top_level_parent("ZUSF02440") == "S5T3")
-
-# another check
-if (debug) find_top_level_parent("ZUSF00641")
+stopifnot(find_top_level_parent("ZUSF02680") == "S5T3")
+stopifnot(find_top_level_parent("ZUSF02675") == "Not found")
 
 # populate parent column with NAs to start
 extractions$original_sample <- NA
 
-# resursively lookup top-level parent of each culture and add to parent column
+# recursively look up top-level parent of each culture and add to parent column
 for (i in seq_len(nrow(extractions))) {
   # because the data are messy we need to first check only for those culture
   # IDs that match the proper naming convention
@@ -52,10 +89,10 @@ for (i in seq_len(nrow(extractions))) {
       # and then we also need to check that they don't occur multiple times
       # in the culturing sheet (due to typos)
       (!extractions$Culture_Label_ID[i] %in%
-       culturing$Label_ID[duplicated(culturing$Label_ID)])) {
+       cultures$Label_ID[duplicated(cultures$Label_ID)])) {
 
     # if both of those conditions are met, then find the parent of the culture
-    # using the recursive lookup function defined above and assign into the
+    # using the recursive look up function defined above and assign into the
     # column 'original_sample' in the extractions data frame
     extractions$original_sample[i] <-
       find_top_level_parent(
@@ -67,10 +104,12 @@ for (i in seq_len(nrow(extractions))) {
 extractions$original_sample[
   extractions$original_sample == "Test Sample"] <- "Test_Sample"
 
-# fix zero padding errors in extraction IDs
+# select out columns of interest
 extractions <- extractions %>%
-  mutate_if(is.factor, as.character) %>%
-  select(Label_ID, original_sample)
+  select(Label_ID, original_sample) %>%
+  filter(original_sample != "Not found") %>%
+  filter(original_sample != "Test_Sample") %>%
+  filter(Label_ID %in% seq_ids_passing_checks$seq_id)
 
 # write out complete csv for use in next script
 write.table(extractions,
